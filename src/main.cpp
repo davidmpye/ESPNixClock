64bd4aa8825a3bab159e8661f1e0bab14bca4e6e
David Pye <davidmpye@gmail.com>
2016-09-19 18:19:44 +0100

    Use last 2 digits of nixie as progress indicator

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,9 @@
 #define LED_PIN 2
 
 const char *ssid = "";
-const char *password = ""; 
+const char *password = "";
+
+const char *firmwareUploadPw = "";
 
 IPAddress ip(192, 168, 1, 15);
 IPAddress gateway(192, 168, 1, 1);
@@ -78,15 +80,17 @@ void setup() {
     ESP.restart();
   });
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    nix0.blank();
+    nix1.blank();
+    nix2.blank();
+    nix3.blank();
+    nix4.blank();
+    nix5.blank();
+    int percent = progress / (total / 100);
+    nix1.displayNumber(percent / 10);
+    nix0.displayNumber(percent % 10);
   });
   ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
   });
 
   WiFi.mode(WIFI_STA);
@@ -94,7 +98,7 @@ void setup() {
   WiFi.begin(ssid, password);
   ArduinoOTA.setPort(8266);
   ArduinoOTA.setHostname("espnixie");
-  ArduinoOTA.setPassword("");
+  ArduinoOTA.setPassword(firmwareUploadPw);
   ArduinoOTA.begin();
 
   //Get the current time from the RTC
