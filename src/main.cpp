5d119b21f63913ed807183bbc45620736ef0d442
davidmpye <davidmpye@gmail.com>
2017-08-04 20:25:15 +0100

    Update to use TZ

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <Adafruit_NeoPixel.h>
 #include <DS3232RTC.h>    //http://github.com/JChristensen/DS3232RTC
 #include <Time.h>
+#include <Timezone.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
@@ -15,7 +16,8 @@
 const char *ssid = "";
 const char *password = "";
 
-const char *firmwareUploadPw = "";
+const char *ota_hostname="espnixie";
+const char *ota_password="";
 
 IPAddress ip(192, 168, 1, 15);
 IPAddress gateway(192, 168, 1, 1);
@@ -29,6 +31,13 @@ Nixie_i2c nix4(4);
 Nixie_i2c nix5(5);
 
  //#define SET_TIME
+
+//Time zone rules
+ TimeChangeRule myDST = {"DST", Last, Sun, Mar, 1, 60};    //Daylight time = UTC+1 hr
+ TimeChangeRule mySTD = {"UTC", Last, Sun, Oct, 2, 0};     //Standard time = UTC
+ Timezone myTZ(myDST, mySTD);
+ time_t currentTime;
+
 
 //Descriptor for the WS2812 LEDs in the nixie boards
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(6, LED_PIN, NEO_GRB + NEO_KHZ800);
@@ -89,16 +98,23 @@ void setup() {
     int percent = progress / (total / 100);
     nix1.displayNumber(percent / 10);
     nix0.displayNumber(percent % 10);
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
   });
   ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
   });
 
   WiFi.mode(WIFI_STA);
   WiFi.config(ip, gateway, subnet);
   WiFi.begin(ssid, password);
   ArduinoOTA.setPort(8266);
-  ArduinoOTA.setHostname("espnixie");
-  ArduinoOTA.setPassword(firmwareUploadPw);
+  ArduinoOTA.setHostname(ota_hostname);
+  ArduinoOTA.setPassword(ota_password);
   ArduinoOTA.begin();
 
   //Get the current time from the RTC
@@ -116,12 +132,18 @@ void setup() {
   setSyncProvider(RTC.get);
   //tube brightness to off
   setNixieBrightness(0);
-  nix5.displayNumber(hour(now())/10);
-  nix4.displayNumber(hour(now())%10);
-  nix3.displayNumber(minute(now())/10);
-  nix2.displayNumber(minute(now())%10);
-  nix1.displayNumber(second(now())/10);
-  nix0.displayNumber(second(now())%10);
+
+    currentTime = myTZ.toLocal(now());
+
+  nix5.displayNumber(hour(currentTime)/10);
+  nix4.displayNumber(hour(currentTime)%10);
+  nix3.displayNumber(minute(currentTime)/10);
+  nix2.displayNumber(minute(currentTime)%10);
+  nix1.displayNumber(second(currentTime)/10);
+  nix0.displayNumber(second(currentTime)%10);
+
+
+
 }
 
 int lastMinute = -1; //used for RGB colour updates
@@ -130,7 +152,8 @@ void loop() {
   //OTA update handler
   ArduinoOTA.handle();
 
-  time_t currentTime = now();
+  currentTime = myTZ.toLocal(now());
+
   int scratch;
   scratch = hour(currentTime) / 10;
   if (nix5.currentNumber() != scratch)  nix5.slotMachineDisplayNumber(scratch);
@@ -177,6 +200,7 @@ void loop() {
       strip.setBrightness(100);
       strip.show();
   }
+  delay(50);
 }
 
 void setColons(bool state) {
